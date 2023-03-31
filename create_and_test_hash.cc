03e3a846c4fec86f43af3c3c55410050e2303efd
BunSoy220 <97924546+BunSoy220@users.noreply.github.com>
2023-03-31 14:11:32 -0400

    Add files via upload

--- create_and_test_hash.cc
+++ create_and_test_hash.cc
@@ -77,6 +77,7 @@ void TestFunctionForHashTable(HashTableType &hash_table,
     cout << "collisions: " << hash_table.Collisions() << endl;
     cout << "avg_collisions: " << float(hash_table.Collisions())/float(hash_table.Elements()) << endl;
     readFile(query_filename, query);
+    cout << endl;
     // for(size_t i = 0; i < query.size(); ++i){
     //     cout << query.at(i) << endl;
     // }
